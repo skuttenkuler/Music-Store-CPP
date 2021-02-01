67cd43badea48dce05e9cc448fb2b2d53923545b
skuttenkuler <sam.kuttenk@gmail.com>
2021-02-01 15:32:34 -0800

    Add project files.

---
+++ Music Store/Music Store.cpp
@@ -0,0 +1,11 @@
+// Music Store.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
