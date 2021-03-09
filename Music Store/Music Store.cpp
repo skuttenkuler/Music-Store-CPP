21d1f18dbcb37b9df389f31c485418f341de0bf3
skuttenkuler <sam.kuttenk@gmail.com>
2021-03-08 16:11:12 -0800

    Delete Item by id, query and deletion

--- Music Store/Music Store.cpp
+++ Music Store/Music Store.cpp
@@ -40,6 +40,7 @@ public:
 void ShowAllItems();
 void ShowInStock();
 void AddItem();
+void RemoveItem();
 
 int main()
 {
@@ -59,8 +60,14 @@ int main()
     std::cout << "Main Menu" << std::endl;
     std::cout << "1. See Inventory." << std::endl;
     std::cout << "2. Currently Stocked Items." << std::endl;
-    std::cout << "3. Add Item to Inventory" << std::endl;
-    std::cout << "4. Exit." << std::endl;
+    std::cout << "3. Add Item to Inventory." << std::endl;
+    std::cout << "4. Delete an Item." << std::endl;
+    //std::cout << "4. Edit Iventory." << std::endl;
+    //std::cout << "5. Display Items Sold." << std::endl;
+    //std::cout << "6. Search Catalog." << std::endl;
+    //std::cout << "7. Create Customer Order." << std::endl;
+    //std::cout << "8. Exit." << std::endl;
+    
     std::cin >> menuChoice;
 
 
@@ -77,6 +84,9 @@ int main()
         AddItem();
         break;
     case 4:
+        RemoveItem();
+        break;
+    case 5:
     ExitProgram:
         //check for input
         std::cout << "Quit? (y/N)";
@@ -199,7 +209,7 @@ void ShowInStock()
 
 
 }
-
+//Add new item
 void AddItem() 
 {
     //initialize
@@ -231,13 +241,11 @@ void AddItem()
     std::cout << "Quantity:" << std::endl;
     std::cin >> quantity;
 
-    std::string price_string(std::to_string(price));
-    std::string quantity_string(std::to_string(quantity));
  
 
     //query string
      //set query string 
-    std::string query = "INSERT INTO music_store_table(artist,album,genre,price,quantity) VALUES ('" + artist + "','" + album + "','" + genre + "','" + price_string + "','" + quantity_string + "')";
+    std::string query = "INSERT INTO music_store_table(artist,album,genre,price,quantity) VALUES ('" + artist + "','" + album + "','" + genre + "','price','quantity')";
 
     const char* q = query.c_str();
     querystate = mysql_query(connection, q);
@@ -264,8 +272,121 @@ void AddItem()
     }
 
 }
-//Add new item
+
 //Remove item
+void RemoveItem()
+{
+    ////clear screen
+    system("CLS");
+
+    char choice;
+    int item_id;
+    //set list of strings
+    std::string items[2000];
+    //id index
+    int id_index;
+    //boolean check
+    bool has_exception = false;
+    bool not_in_database = false;
+
+    std::cout << "Welcome To Music Store" << std::endl;
+    std::cout << "Remove Item Menu" << std::endl ;
+    //set query string
+    std::string query = "SELECT id, artist, album FROM music_store_table";
+    const char* q = query.c_str();
+    querystate = mysql_query(connection, q);
+    if (!querystate)
+    {
+        res = mysql_store_result(connection);
+        //display items for user
+        std::cout << "ID\tARTIST\tALBUM\n" << std::endl;
+        while (row = mysql_fetch_row(res))
+        {
+            std::cout << row[0] << "\t" << row[1] << "\t" << row[2] << std::endl;
+            //assign id index to list with items from id row, then increment
+            items[id_index] = row[0];
+            id_index++;
+        }
+    }
+    else
+    {
+        std::cout << "Query Issue" << mysql_errno(connection) << std::endl;
+    }
+    //try function to select get user input of id, if error catch and throw error
+    try
+    {
+        std::cout << std::endl;
+        std::cout << "Enter Item ID:" << std::endl;
+        std::cin >> item_id;
+        std::cout << std::endl;
+    }
+    catch(std::exception e)
+    {
+        std::cout << "Please Enter Valid Selection." << std::endl;
+        has_exception = true;
+        goto ExitProgram;
+    }
+
+    if (has_exception == false)
+    {
+        std::stringstream stream_id;
+        std::string str_id;
+        stream_id << item_id;
+        stream_id >> str_id;
+        //loop through table and find id, if found set true, else break
+        for (int i = 0; i < id_index; i++)
+        {
+            if (str_id != items[i])
+            {
+                not_in_database = true;
+            }
+            else
+            {
+                not_in_database = false;
+                break;
+            }
+        }
+        //if id found
+        if (not_in_database == false)
+        {
+            std::string delete_query = "DELETE FROM music_store_table WHERE id = '" + str_id + "'";
+            const char* qd = delete_query.c_str();
+            querystate = mysql_query(connection, qd);
+
+            if (!querystate)
+            {
+                std::cout << "Deletion Successful!" << std::endl;
+
+            }
+            else
+            {
+                std::cout << "Deletion Failed." << mysql_errno(connection) << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Item Not Found." << std::endl;
+        }
+    }
+    // Exit back to menu
+    ExitProgram:
+    std::cout << "Press 'm' to return to main menu." << std::endl;
+    std::cout << "Press 'd' to delete another item." << std::endl;
+    std::cout << "Press anyother key to exit." << std::endl;
+    std::cin >> choice;
+    if (choice == 'm' || choice == 'M')
+    {
+        main();
+    }
+    else if (choice == 'd' || choice == 'D')
+    {
+        RemoveItem();
+    }
+    else
+    {
+        exit(0);
+    }
+}
 //edit item
 //show items sold
 //search catalog
